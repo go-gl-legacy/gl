4e432fe1af3cd66903eee20e422d31a46083ff06
Your Name Comes Here <banthar@gmail.com>
2009-11-13 17:45:19 +0100

    gofmt README

--- test-gl.go
+++ test-gl.go
@@ -12,41 +12,37 @@ func main() {
 
 	sdl.SetVideoMode(640, 480, 32, sdl.OPENGL);
 
-    if(gl.Init() != 0)
-    {
-        panic("glew error");
-    }
+	if gl.Init() != 0 {
+		panic("glew error")
+	}
 
 	var running = true;
 
-	for running
-    {
+	for running {
 
 		e := &sdl.Event{};
 
-		for (e.Poll())
-        {
-            switch(e.Type)
-            {
-                case sdl.QUIT:
-    				running = false;
-                    break;
-                case sdl.KEYDOWN:
-                    running = false;
-                    break;
-                default:
-                    fmt.Printf("Event: %08x\n",e.Type);
-            }
+		for e.Poll() {
+			switch e.Type {
+			case sdl.QUIT:
+				running = false;
+				break;
+			case sdl.KEYDOWN:
+				running = false;
+				break;
+			default:
+				fmt.Printf("Event: %08x\n", e.Type)
+			}
 		}
 
-        gl.Begin(gl.TRIANGLES);
-            gl.Vertex3f(0,0,0);
-            gl.Vertex3f(0,1,0);
-            gl.Vertex3f(1,1,0);
-        gl.End();
+		gl.Begin(gl.TRIANGLES);
+		gl.Vertex3f(0, 0, 0);
+		gl.Vertex3f(0, 1, 0);
+		gl.Vertex3f(1, 1, 0);
+		gl.End();
 
 		sdl.GL_SwapBuffers();
-        sdl.Delay(25);
+		sdl.Delay(25);
 	}
 
 	sdl.Quit();
--- test-sdl.go
+++ test-sdl.go
@@ -4,7 +4,7 @@ import "sdl"
 //import "math"
 
 import "fmt"
-import "image/png";
+import "image/png"
 import "os"
 
 func main() {
@@ -25,32 +25,30 @@ func main() {
 
 		e := &sdl.Event{};
 
-		for (e.Poll())
-        {
-            switch(e.Type)
-            {
-                case sdl.QUIT:
-                    file,err := os.Open("shoot.png",os.O_CREATE|os.O_WRONLY,0766);
-                    println(err);
-                    png.Encode(file, screen);
-                    file.Close();
-    				running = false;
-                    break;
-                case sdl.KEYDOWN:
-                    println(e.Keyboard().Keysym.Sym);
-                case sdl.MOUSEBUTTONDOWN:
-                    println("Click:",e.MouseButton().X,e.MouseButton().Y);
-                    x=int16(e.MouseButton().X);
-                    y=int16(e.MouseButton().Y);
-                default:
-                    fmt.Printf("Event: %08x\n",e.Type);
-            }
+		for e.Poll() {
+			switch e.Type {
+			case sdl.QUIT:
+				file, err := os.Open("shoot.png", os.O_CREATE|os.O_WRONLY, 0766);
+				println(err);
+				png.Encode(file, screen);
+				file.Close();
+				running = false;
+				break;
+			case sdl.KEYDOWN:
+				println(e.Keyboard().Keysym.Sym)
+			case sdl.MOUSEBUTTONDOWN:
+				println("Click:", e.MouseButton().X, e.MouseButton().Y);
+				x = int16(e.MouseButton().X);
+				y = int16(e.MouseButton().Y);
+			default:
+				fmt.Printf("Event: %08x\n", e.Type)
+			}
 		}
 
 		screen.FillRect(nil, 0x302019);
 		screen.Blit(&sdl.Rect{x, y, 0, 0}, image, nil);
 		screen.Flip();
-        sdl.Delay(25);
+		sdl.Delay(25);
 	}
 
 	image.Free();
