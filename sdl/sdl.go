0ce72970cf675f16a908789c7eaf6baae73768c3
Your Name Comes Here <banthar@gmail.com>
2009-11-23 16:29:09 +0100

    a few more SDL functions

--- sdl/sdl.go
+++ sdl/sdl.go
@@ -16,8 +16,10 @@ type cast unsafe.Pointer
 
 //SDL
 
+func GetError() string	{ return C.GoString(C.SDL_GetError()) }
+
 func Init(flags uint32) int	{ return int(C.SDL_Init(C.Uint32(flags))) }
-func Quit()	{ C.SDL_Quit() }
+func Quit()			{ C.SDL_Quit() }
 
 //SDL_video
 
@@ -26,19 +28,26 @@ func SetVideoMode(w int, h int, bpp int, flags uint32) *Surface {
 	return (*Surface)(cast(screen));
 }
 
+func GetVideoSurface() *Surface	{ return (*Surface)(cast(C.SDL_GetVideoSurface())) }
+
+
+func VideoModeOK(width int, height int, bpp int, flags uint32) int {
+	return int(C.SDL_VideoModeOK(C.int(width), C.int(height), C.int(bpp), C.Uint32(flags)))
+}
+
+func SDL_UpdateRect(screen *Surface, x int32, y int32, w uint32, h uint32) {
+	C.SDL_UpdateRect((*C.SDL_Surface)(cast(screen)), C.Sint32(x), C.Sint32(y), C.Uint32(w), C.Uint32(h))
+}
+
 func WM_SetCaption(title string, icon string) {
-    ctitle:=C.CString(title);
-    cicon:=C.CString(icon);
-    C.SDL_WM_SetCaption(ctitle,cicon);
-    C.free(unsafe.Pointer(ctitle));
-    C.free(unsafe.Pointer(cicon));
+	ctitle := C.CString(title);
+	cicon := C.CString(icon);
+	C.SDL_WM_SetCaption(ctitle, cicon);
+	C.free(unsafe.Pointer(ctitle));
+	C.free(unsafe.Pointer(cicon));
 }
 
-func GL_SwapBuffers()
-{
-    C.SDL_GL_SwapBuffers();
-}
-
+func GL_SwapBuffers()	{ C.SDL_GL_SwapBuffers() }
 
 func (screen *Surface) Flip() int	{ return int(C.SDL_Flip((*C.SDL_Surface)(cast(screen)))) }
 
@@ -49,11 +58,6 @@ func (screen *Surface) Lock() int {
 }
 
 func (screen *Surface) Unlock() int	{ return int(C.SDL_Flip((*C.SDL_Surface)(cast(screen)))) }
-
-func GetVideoSurface() *Surface {
-	var screen = C.SDL_GetVideoSurface();
-	return (*Surface)(cast(screen));
-}
 
 func (dst *Surface) Blit(dstrect *Rect, src *Surface, srcrect *Rect) int {
 	var ret = C.SDL_UpperBlit(
@@ -76,46 +80,105 @@ func (dst *Surface) FillRect(dstrect *Rect, color uint32) int {
 
 // surface --> Image
 
-func (surface *Surface) ColorModel() image.ColorModel
-{
-    return nil;
+func (surface *Surface) ColorModel() image.ColorModel {
+	//TODO
+	return nil
 }
 
-func (surface *Surface) Width() int
-{
-    return int(surface.W);
-}
+func (surface *Surface) Width() int	{ return int(surface.W) }
 
-func (surface *Surface) Height() int
-{
-    return int(surface.H);
-}
+func (surface *Surface) Height() int	{ return int(surface.H) }
 
-func (surface *Surface) At(x, y int) image.Color
-{
+func (surface *Surface) Set(x, y int, c image.Color) {
+	//TODO endianess, bpp, alpha, etc
 
-    //TODO endianess, bpp, alpha, etc
+	var bpp = int(surface.Format.BytesPerPixel);
 
-    var bpp = int(surface.Format.BytesPerPixel);
+	var pixel = uintptr(unsafe.Pointer(surface.Pixels));
 
-    var pixel=uintptr(unsafe.Pointer(surface.Pixels));
+	pixel += uintptr(y*int(surface.Pitch) + x*bpp);
 
-    pixel += uintptr( y * int(surface.Pitch) + x * bpp);
+	var p = (*image.RGBAColor)(unsafe.Pointer(pixel));
 
-    var color= *((*image.RGBAColor)(unsafe.Pointer(pixel)));
+	var r, g, b, a = c.RGBA();
 
-    return image.RGBAColor{color.B,color.G,color.R,255-color.A};
+	p.R = uint8(r);
+	p.G = uint8(g);
+	p.R = uint8(b);
+	p.A = uint8(255 - a);
 
 }
+
+
+func (surface *Surface) At(x, y int) image.Color {
+
+	var bpp = int(surface.Format.BytesPerPixel);
+
+	var pixel = uintptr(unsafe.Pointer(surface.Pixels));
+
+	pixel += uintptr(y*int(surface.Pitch) + x*bpp);
+
+	var color = *((*uint32)(unsafe.Pointer(pixel)));
+
+	var r C.Uint8;
+	var g C.Uint8;
+	var b C.Uint8;
+	var a C.Uint8;
+
+
+	C.SDL_GetRGBA(C.Uint32(color),(*C.SDL_PixelFormat)(cast(surface.Format)),&r,&g,&b,&a);
+
+	return image.RGBAColor{uint8(r),uint8(g),uint8(b),uint8(a)};
+
+}
+
 
 //SDL image
 
 func Load(file string) *Surface {
-    cfile := C.CString(file);
+	cfile := C.CString(file);
 	var screen = C.IMG_Load(cfile);
-    C.free(unsafe.Pointer(cfile));
+	C.free(unsafe.Pointer(cfile));
 	return (*Surface)(cast(screen));
 }
+
+// SDL keys
+
+func EnableUNICODE(enable int) int	{ return int(C.SDL_EnableUNICODE(C.int(enable))) }
+
+func EnableKeyRepeat(delay int, interval int) int {
+	return int(C.SDL_EnableKeyRepeat(C.int(delay), C.int(interval)))
+}
+
+func GetKeyRepeat() (int, int) {
+
+	var delay int;
+	var interval int;
+
+	C.SDL_GetKeyRepeat((*C.int)(cast(&delay)), (*C.int)(cast(&interval)));
+
+	return delay, interval;
+
+}
+
+// TODO
+// Uint8 * SDL_GetKeyState(int *numkeys)
+// func GetKeyState(numkeys *int) *uint8
+// {
+// 	return (*uint8)(C.SDL_GetKeyState((*C.int)(numkeys)));
+// }
+
+type Mod C.int
+type Key C.int
+
+// SDLMod SDL_GetModState(void)
+func GetModState() Mod	{ return Mod(C.SDL_GetModState()) }
+
+// void SDL_SetModState(SDLMod modstate)
+func SetModState(modstate Mod)	{ C.SDL_SetModState(C.SDLMod(modstate)) }
+
+// char * SDL_GetKeyName(SDLKey key)
+func GetKeyName(key Key) string	{ return C.GoString(C.SDL_GetKeyName(C.SDLKey(key))) }
 
 //SDL events
 
@@ -129,38 +192,30 @@ func (event *Event) Poll() bool {
 	return ret != 0;
 }
 
-func (event *Event) Keyboard() *KeyboardEvent
-{
-    if(event.Type==KEYUP || event.Type==KEYDOWN)
-    {
-        return (*KeyboardEvent)(cast(event));
-    }
+func (event *Event) Keyboard() *KeyboardEvent {
+	if event.Type == KEYUP || event.Type == KEYDOWN {
+		return (*KeyboardEvent)(cast(event))
+	}
 
-    return nil;
+	return nil;
 }
 
-func (event *Event) MouseButton() *MouseButtonEvent
-{
-    if(event.Type==MOUSEBUTTONDOWN || event.Type==MOUSEBUTTONUP)
-    {
-        return (*MouseButtonEvent)(cast(event));
-    }
+func (event *Event) MouseButton() *MouseButtonEvent {
+	if event.Type == MOUSEBUTTONDOWN || event.Type == MOUSEBUTTONUP {
+		return (*MouseButtonEvent)(cast(event))
+	}
 
-    return nil;
+	return nil;
 }
 
-func (event *Event) MouseMotion() *MouseMotionEvent
-{
-    if(event.Type==MOUSEMOTION)
-    {
-        return (*MouseMotionEvent)(cast(event));
-    }
+func (event *Event) MouseMotion() *MouseMotionEvent {
+	if event.Type == MOUSEMOTION {
+		return (*MouseMotionEvent)(cast(event))
+	}
 
-    return nil;
+	return nil;
 }
 
 //SDL time
 
 func Delay(ms uint32)	{ C.SDL_Delay(C.Uint32(ms)) }
-
-
