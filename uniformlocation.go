4bed508c241630926ae9494ad9ab3bfd7210cf51
Jim Arnold <jim@thoughtworks.com>
2012-12-17 09:07:10 -0500

    panic if Uniform*v arrays are empty

--- uniformlocation.go
+++ uniformlocation.go
@@ -25,6 +25,9 @@ func (location UniformLocation) Uniform3f(x float32, y float32, z float32) {
 }
 
 func (location UniformLocation) Uniform1fv(count int, v []float32) {
+	if len(v) < 1 {
+		panic("Invalid array length - must be at least 1")
+	}
 	C.glUniform1fv(C.GLint(location), C.GLsizei(count), (*C.GLfloat)(ptr(v)))
 }
 
@@ -33,10 +36,16 @@ func (location UniformLocation) Uniform1i(x int) {
 }
 
 func (location UniformLocation) Uniform1iv(count int, v []int32) {
+	if len(v) < 1 {
+		panic("Invalid array length - must be at least 1")
+	}
 	C.glUniform1iv(C.GLint(location), C.GLsizei(count), (*C.GLint)(&v[0]))
 }
 
 func (location UniformLocation) Uniform2fv(count int, v []float32) {
+	if len(v) < 1 {
+		panic("Invalid array length - must be at least 1")
+	}
 	C.glUniform2fv(C.GLint(location), C.GLsizei(count), (*C.GLfloat)(&v[0]))
 }
 
@@ -45,10 +54,16 @@ func (location UniformLocation) Uniform2i(x int, y int) {
 }
 
 func (location UniformLocation) Uniform2iv(count int, v []int32) {
+	if len(v) < 1 {
+		panic("Invalid array length - must be at least 1")
+	}
 	C.glUniform2iv(C.GLint(location), C.GLsizei(count), (*C.GLint)(&v[0]))
 }
 
 func (location UniformLocation) Uniform3fv(count int, v []float32) {
+	if len(v) < 1 {
+		panic("Invalid array length - must be at least 1")
+	}
 	C.glUniform3fv(C.GLint(location), C.GLsizei(count), (*C.GLfloat)(&v[0]))
 }
 
@@ -57,6 +72,9 @@ func (location UniformLocation) Uniform3i(x int, y int, z int) {
 }
 
 func (location UniformLocation) Uniform3iv(count int, v []int32) {
+	if len(v) < 1 {
+		panic("Invalid array length - must be at least 1")
+	}
 	C.glUniform3iv(C.GLint(location), C.GLsizei(count), (*C.GLint)(&v[0]))
 }
 
@@ -65,6 +83,9 @@ func (location UniformLocation) Uniform4f(x float32, y float32, z float32, w flo
 }
 
 func (location UniformLocation) Uniform4fv(count int, v []float32) {
+	if len(v) < 1 {
+		panic("Invalid array length - must be at least 1")
+	}
 	C.glUniform4fv(C.GLint(location), C.GLsizei(count), (*C.GLfloat)(&v[0]))
 }
 
@@ -73,5 +94,8 @@ func (location UniformLocation) Uniform4i(x int, y int, z int, w int) {
 }
 
 func (location UniformLocation) Uniform4iv(count int, v []int32) {
+	if len(v) < 1 {
+		panic("Invalid array length - must be at least 1")
+	}
 	C.glUniform4iv(C.GLint(location), C.GLsizei(count), (*C.GLint)(&v[0]))
 }
