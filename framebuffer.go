1f919f8f3ea86b7ea07e1cf3b7a646afa7a586c6
Emil Arfvidsson <emil@arfvidsson.com>
2013-06-24 10:23:17 -0700

    Add more explanatory doc for Bind, BindTarget, Unbind and UnbindTarget.

--- framebuffer.go
+++ framebuffer.go
@@ -13,21 +13,28 @@ type Framebuffer Object
 
 // void glBindFramebuffer(GLenum target, GLuint framebuffer);
 //
-// To bind a Framebuffer to a specific target, see BindTarget.
+// Binds fb to target FRAMEBUFFER. To bind to a specific target, see BindTarget.
 func (fb Framebuffer) Bind() {
 	C.glBindFramebuffer(C.GLenum(FRAMEBUFFER), C.GLuint(fb))
 }
 
-// void glBindFramebuffer(GLenum target, GLuint framebuffer);
+// Binds fb to the specified target.
+//
+// See issue at github for why this function exists:
+// http://github.com/go-gl/gl/issues/113
 func (fb Framebuffer) BindTarget(target GLenum) {
 	C.glBindFramebuffer(C.GLenum(target), C.GLuint(fb))
 }
 
-// To unbind a Framebuffer from a specific target, see UnbindTarget.
+// Unbinds target FRAMEBUFFER. To unbind a a specific target, see UnbindTarget.
 func (fb Framebuffer) Unbind() {
 	C.glBindFramebuffer(C.GLenum(FRAMEBUFFER), 0)
 }
 
+// Unbinds the specified target.
+//
+// See issue at github for why this function exists:
+// http://github.com/go-gl/gl/issues/113
 func (fb Framebuffer) UnbindTarget(target GLenum) {
 	C.glBindFramebuffer(C.GLenum(target), 0)
 }
